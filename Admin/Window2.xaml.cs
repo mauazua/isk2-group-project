b81087d2c75197e4a4781e134d67ef13cb3a5acd
michalwysocki <michaljerzywysocki@wp.pl>
2016-01-15 00:17:19 +0100

    Poprawienie requestow, nadal na sztywno, bo bylo testowane na wewnetrznym serwerze z nasza aplikacja ruby.

--- Admin/Window2.xaml.cs
+++ Admin/Window2.xaml.cs
@@ -1,37 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
+using System.Runtime.Serialization.Json;
+using System.Text;
 using System.Windows.Controls;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+using Npgsql;
+using System.Runtime.Serialization;
 
 namespace Admin
 {
-    /// <summary>
-    /// Interaction logic for Window2.xaml
-    /// </summary>
+
     public partial class Window2 : Window
     {
         public Window2()
         {
             InitializeComponent();
-        }
 
-       
+        }
 
         private void Wyślij_Click(object sender, RoutedEventArgs e)
         {
-            string name = textBox.ToString();
-            
+
             var webAddr = "http://192.168.23.128:3000/api/events";
             var httpWebRequest = (HttpWebRequest)WebRequest.Create(webAddr);
             httpWebRequest.ContentType = "application/json; charset=utf-8";
@@ -40,8 +43,20 @@ namespace Admin
             using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
             {
 
-                
-                streamWriter.Write(name);
+
+
+                Trip nowa = new Trip();
+                nowa.name = textBox.Text;
+                nowa.start_date = datepicker1.Text;
+                nowa.end_date = datepicker2.Text;
+                nowa.location = textBox_Copy.Text;
+                nowa.country = textBox_Copy1.Text;
+                nowa.price = int.Parse(textBox_Copy2.Text);
+                nowa.capacity = int.Parse(textBox_Copy3.Text);
+
+
+                string jsonString = JsonHelper.JsonSerializer(nowa);
+                streamWriter.Write(jsonString);
                 streamWriter.Flush();
             }
 
@@ -49,8 +64,10 @@ namespace Admin
             using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
             {
                 var result = streamReader.ReadToEnd();
-                MessageBox.Show(result);
+                System.Windows.MessageBox.Show(result);
             }
         }
+
+
     }
 }
