9cf9d06dc3a0ff8d329be7a0924d192f42605d65
pruszek <adam.truszkowski@gmail.com>
2016-01-15 16:24:16 +0100

    dodanie nowych okien i poprawienie obecnych

---
+++ Admin/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+using Npgsql;
+namespace Admin
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void zalogujClick(object sender, RoutedEventArgs e)
+        {
+            if (loginTextBox.Text != "" && passTextBox.Password != "")
+            {
+                /*
+                NpgsqlConnection con = new NpgsqlConnection("Server=localhost;Port=5432;User Id=postgres;Password=miszamisza1; Database=AdminLogin;");
+                con.Open();
+
+                NpgsqlCommand com = new NpgsqlCommand("Select * from admindata where login='@UserId' and password='@word';",con);
+                com.Parameters.AddWithValue("@UserId", loginTextBox.Text);
+                com.Parameters.AddWithValue("@word", passTextBox.Password);
+                com.CommandType = CommandType.Text;
+                NpgsqlDataAdapter adapt = new NpgsqlDataAdapter(com);
+                adapt.SelectCommand = com;
+                DataSet ds = new DataSet();
+                
+                adapt.Fill(ds);
+                con.Close();
+                int count = ds.Tables[0].Rows.Count;
+                if (count==1)
+                 
+                 !!!!PS: PÓKI CO POBIERANIE LOGINU I HASŁA Z BAZY NIE DZIAŁA - TRWAJĄ PRACE NAD TYM!!!!
+                 */
+
+                if (loginTextBox.Text == "a" && passTextBox.Password == "a")
+                {
+                    Window1 obj = new Window1();
+
+                    obj.Show(); //otwiera się nowe okno 
+                    this.Hide();//okno logowania ukrywa się po poprawnym zalogowaniu
+                }
+                else
+                {
+                    MessageBox.Show("Niepoprawna nazwa użytkownika lub hasło");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nazwa użytkownika i hasło są wymagane");
+            }
+        }
+    }
+}
---
+++ Admin/Window1.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+using Npgsql;
+
+namespace Admin
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        private void logoutClick(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            MessageBox.Show("Zostałeś poprawnie wylogowany");
+        }
+        private void ProgramInfo(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Panel Administracyjny - Program został opracowany przez ISK2, która jest studencką grupą, starającą się zapewnić w pełni działającą i stabilną aplikację desktopową do administracji wycieczek ");  
+        }
+        private void Dodaj_Wycieczke(object sender, RoutedEventArgs e)
+        {
+            Window2 obj = new Window2();
+
+            obj.Show();
+        }
+        private void Lista_Rezerwacji(object sender, RoutedEventArgs e)
+        {
+            Window3 obj = new Window3();
+
+            obj.Show();
+        }
+        private void Potwierdz_Rezerwacje(object sender, RoutedEventArgs e)
+        {
+            Window5 okno = new Window5();
+
+            okno.Show();
+        }
+        private void Lista_Wycieczek(object sender, RoutedEventArgs e)
+        {
+            Window4 obj = new Window4();
+
+            obj.Show();
+        }
+    }
+}
