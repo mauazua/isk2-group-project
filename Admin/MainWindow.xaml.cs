0885a821b25a62f5c23a4f6daf061a6224226645
mateusz24 <mateusz.orlowski28@gmail.com>
2016-02-12 10:37:43 +0100

    Dodawanie Kliento

--- Admin/MainWindow.xaml.cs
+++ Admin/MainWindow.xaml.cs
@@ -10,30 +10,21 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Data.SqlClient;
-using System.Data;
-using Npgsql;
 namespace Admin
 {
-    
     public partial class MainWindow : Window
     {
-
-        
         public MainWindow()
         {
             InitializeComponent();
+            TextBoxLogin.Focus();
         }
-
-        private void zalogujClick(object sender, RoutedEventArgs e)
+        private void ButtonZaloguj(object sender, RoutedEventArgs e)
         {
-            if (loginTextBox.Text != "" && passTextBox.Password != "")
+            if (TextBoxLogin.Text != "" && PasswordBoxHaslo.Password != "")
             {
-
-                /*
-                NpgsqlConnection con = new NpgsqlConnection("Server=localhost;Port=5432;User Id=postgres;Password=miszamisza1; Database=AdminLogin;");
+                /*NpgsqlConnection con = new NpgsqlConnection("Server=localhost;Port=5432;User Id=postgres;Password=miszamisza1; Database=AdminLogin;");
                 con.Open();
 
                 NpgsqlCommand com = new NpgsqlCommand("Select * from admindata where login='@UserId' and password='@word';",con);
@@ -49,39 +40,22 @@ namespace Admin
                 int count = ds.Tables[0].Rows.Count;
                 if (count==1)
                  
-                 !!!!PS: PÓKI CO POBIERANIE LOGINU I HASŁA Z BAZY NIE DZIAŁA - TRWAJĄ PRACE NAD TYM!!!!
-                 */
-                 
-                if (loginTextBox.Text=="administrator" && passTextBox.Password=="123qweQWE" )
-
+                 PÓKI CO POBIERANIE LOGINU I HASŁA Z BAZY NIE DZIAŁA - TRWAJĄ PRACE NAD TYM*/
+                if (TextBoxLogin.Text == "1" && PasswordBoxHaslo.Password == "1")
                 {
-                                                    
-                    Window1 obj = new Window1();
-
-                    
-                    obj.Show(); //otwiera się nowe okno 
-                    this.Hide();//okno logowania ukrywa się po poprawnym zalogowaniu
-
-
+                    PanelAdministracyjny obj = new PanelAdministracyjny();
+                    obj.Show();
+                    this.Close();
                 }
                 else
                 {
-
-                    MessageBox.Show("Niepoprawna nazwa użytkownika lub hasło");
-
+                    MessageBox.Show("Nieprawidłowy login lub hasło");
                 }
             }
             else
             {
-
-                MessageBox.Show("Nazwa użytkownika i hasło są wymagane");
-
-            }   
-
-        }
-
-                
-            
+                MessageBox.Show("Wpisz login i hasło");
             }
-
+        }
+    }
 }
