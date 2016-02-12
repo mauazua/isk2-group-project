14960a10c1a9f1f64c421574042052b6a2c64c81
heduwer <arekstepniewski2@gmail.com>
2016-02-12 22:41:58 +0100

    jako admin mogę anulować zapis użytkownika na liście zapisów #3

---
+++ Admin/MainMenu.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Threading;
+
+namespace Admin
+{
+    public partial class MainMenu : Window
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void logoutClick(object sender, RoutedEventArgs e)
+        {
+            Hide();
+            MessageBox.Show("Zostałeś poprawnie wylogowany");
+            
+        }
+        private void ProgramInfo(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Panel Administracyjny");  
+        }
+
+        private void Usun_Rezerwacje(object sender, RoutedEventArgs e)
+        {
+
+            UsunRezerwacje usun = new UsunRezerwacje();
+            usun.Show();
+
+        }
+    }
+}
