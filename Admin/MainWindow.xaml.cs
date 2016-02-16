bf6549a6e8d5cef3c4e94439f36b6b47bf989c4b
pruszek <adam.truszkowski@gmail.com>
2016-02-16 15:25:03 +0100

    przywrócenie do stanu sprzed uszkodzenia

--- Admin/MainWindow.xaml.cs
+++ Admin/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,62 +10,52 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Data.SqlClient;
-using System.Data;
-using System.Configuration;
-using System.Security;
-using System.Security.Cryptography;
 namespace Admin
 {
-
     public partial class MainWindow : Window
     {
-
-
         public MainWindow()
         {
             InitializeComponent();
+            TextBoxLogin.Focus();
         }
-
-        private void zalogujClick(object sender, RoutedEventArgs e)
+        private void ButtonZaloguj(object sender, RoutedEventArgs e)
         {
-            if (loginTextBox.Text != "" && passTextBox.Password != "")
+            if (TextBoxLogin.Text != "" && PasswordBoxHaslo.Password != "")
             {
+                /*NpgsqlConnection con = new NpgsqlConnection("Server=localhost;Port=5432;User Id=postgres;Password=miszamisza1; Database=AdminLogin;");
+                con.Open();
 
-
-                if (loginTextBox.Text == System.Configuration.ConfigurationManager.AppSettings["username"] && passTextBox.Password == System.Configuration.ConfigurationManager.AppSettings["password"])
-
-
+                NpgsqlCommand com = new NpgsqlCommand("Select * from admindata where login='@UserId' and password='@word';",con);
+                com.Parameters.AddWithValue("@UserId", loginTextBox.Text);
+                com.Parameters.AddWithValue("@word", passTextBox.Password);
+                com.CommandType = CommandType.Text;
+                NpgsqlDataAdapter adapt = new NpgsqlDataAdapter(com);
+                adapt.SelectCommand = com;
+                DataSet ds = new DataSet();
+                
+                adapt.Fill(ds);
+                con.Close();
+                int count = ds.Tables[0].Rows.Count;
+                if (count==1)
+                 
+                 PÓKI CO POBIERANIE LOGINU I HASŁA Z BAZY NIE DZIAŁA - TRWAJĄ PRACE NAD TYM*/
+                if (TextBoxLogin.Text == "1" && PasswordBoxHaslo.Password == "1")
                 {
-
-                    Window1 obj = new Window1();
-
-
-                    obj.Show(); //otwiera się nowe okno
-                    this.Hide();//okno logowania ukrywa się po poprawnym zalogowaniu
-
-
+                    PanelAdministracyjny obj = new PanelAdministracyjny();
+                    obj.Show();
+                    this.Close();
                 }
                 else
                 {
-
-                    MessageBox.Show("Niepoprawna nazwa użytkownika lub hasło");
-
+                    MessageBox.Show("Nieprawidłowy login lub hasło");
                 }
             }
             else
             {
-
-                MessageBox.Show("Nazwa użytkownika i hasło są wymagane");
-
+                MessageBox.Show("Wpisz login i hasło");
             }
-
         }
-
-
-
-            }
-
+    }
 }
