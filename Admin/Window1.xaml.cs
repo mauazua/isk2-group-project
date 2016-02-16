6dec486d224d44797800353bdf7ff3eef41dceeb
pruszek <adam.truszkowski@gmail.com>
2016-02-16 14:43:32 +0100

    naprawienie błędów

--- Admin/Window1.xaml.cs
+++ Admin/Window1.xaml.cs
@@ -13,7 +13,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Data.SqlClient;
 using System.Data;
 using Npgsql;
